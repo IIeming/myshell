9097170fa903e72190cbea8ead8d5c37ab32b619
Ailemon <44798936+luckynmboy@users.noreply.github.com>
2019-04-17 18:44:48 +0800

    Update initserver.sh

--- initserver.sh
+++ initserver.sh
@@ -5,6 +5,7 @@ yum install vim -y
 yum install autofs -y
 yum install lrzsz -y
 yum install bash-completion -y
+yum install net-tools-2.0-0.24.20131004git.el7.x86_64
 systemctl start autofs
 systemctl enable autofs
 #关闭防火墙

af07a3db91d4d8ac385a2809fb285db72565ff91
Ailemon <44798936+luckynmboy@users.noreply.github.com>
2019-04-17 18:43:04 +0800

    Create kvm_ip.sh

---
+++ kvm_ip.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#注意事项:如果报错说没有arp命令的话,需要
+#yum  install   net-tools-2.0-0.24.20131004git.el7.x86_64
+#如果查询的虚拟主机,没有返回数据,可能是虚拟机配置有问题,没有生成ip地址
+#2019_4_17  
+kvm_hanshu () {
+case  $name  in
+exit)
+        echo $name
+        exit
+        ;;
+$name)
+        mac=`virsh dumpxml $name |grep -w mac |awk -F "'" '{print $2}'`
+        echo "虚拟机$name,的ip地址是:`arp -a |grep $mac | awk -F"[()]" '{print $2}'` "
+        ;;
+esac
+}
+########
+echo  "请选择要查询的虚拟机名称"
+select  name in `virsh list |awk -F" " '{print $2}'|grep -v "Name" |sort |uniq -u ` "exit" ; do
+kvm_hanshu
+done
+--------------------- 
+作者：哼嘿哈嘿 
+来源：CSDN 
+原文：https://blog.csdn.net/weixin_44208042/article/details/89362058 
+版权声明：本文为博主原创文章，转载请附上博文链接！
